7417d6bcc0b1de86014bbf86251579d68e3580ed
AlexNicolici <alexandru_nicolici@yahoo.com>
2023-09-18 09:39:49 +0300

    created auth context and initial state

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import { Fragment } from "react";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </Fragment>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 };
 
