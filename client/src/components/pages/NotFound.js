997393c59fdb12e09f6cbdc02e3f7cc172626a01
AlexNicolici <alexandru_nicolici@yahoo.com>
2023-09-18 11:05:32 +0300

    added NotFound page

---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1 style={{ color: "red", display: "flex", justifyContent: "center" }}>
+        Not found!
+      </h1>
+    </div>
+  );
+}
+
+export default NotFound;
