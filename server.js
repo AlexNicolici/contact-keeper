9d387b93007fa3aa727c19d44e3ae9a9c88ffe7b
AlexNicolici <alexandru_nicolici@yahoo.com>
2023-09-25 10:25:07 +0300

    remove heroku server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const path = require("path");
 
 const app = express();
 
@@ -15,15 +14,9 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-// Serve static assets in production
-if (process.end.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+app.get("/", (req, res) =>
+  res.send({ msg: "Welcome to the ContactKeeper API.." })
+);
 
 const PORT = process.env.PORT || 5000;
 
